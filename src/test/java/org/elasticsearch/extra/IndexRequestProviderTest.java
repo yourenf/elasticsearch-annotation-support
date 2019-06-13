83604c1f1937fae828cd98abe8cf0a8fe04d7b64
yourenf <346873404@qq.com]>
2019-06-13 18:07:15 +0800

    add test

--- src/test/java/org/elasticsearch/extra/IndexRequestProviderTest.java
+++ src/test/java/org/elasticsearch/extra/IndexRequestProviderTest.java
@@ -1,8 +1,12 @@
 package org.elasticsearch.extra;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+import com.fasterxml.jackson.databind.util.Converter;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.bytes.BytesReference;
@@ -11,17 +15,15 @@ import org.elasticsearch.extra.context.annotation.Routing;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.IOException;
+import java.util.*;
 
 import static org.junit.Assert.*;
 
 public class IndexRequestProviderTest {
 
   @Test
-  public void create() throws JsonProcessingException {
+  public void create() throws IOException {
     ObjectMapper mapper = new ObjectMapper();
     IndexRequestConfig config = new IndexRequestConfig(value -> {
       try {
@@ -31,23 +33,23 @@ public class IndexRequestProviderTest {
       }
     });
     IndexRequestProvider<PModel> provider = config.createProvider(PModel.class);
-
-    IndexRequest request = provider.create(new PModel("1", 2), "ss");
+    PModel model = new PModel("1", 2);
+    IndexRequest request = provider.create(model, "ss");
     BytesReference source = request.source();
-    Map<String, Object> expect = new LinkedHashMap<>();
-    expect.put("value", "1");
-    expect.put("value2", 2);
-    IModel m = new IModel("1", 2);
-    expect.put("model", m);
-    expect.put("modelArray", Arrays.asList(m, m, m));
-    Assert.assertEquals(mapper.writeValueAsString(expect), source.utf8ToString());
+    PModel value = mapper.readValue(source.utf8ToString(), PModel.class);
+    Assert.assertTrue(model.equals(value));
   }
 
   public static final class PModel {
+    @JsonSerialize(converter = JsonConverterTest.PinyinCounter.class)
+    @JsonDeserialize(converter = JsonConverterTest.DePinyinCounter.class)
     private String value;
     private Integer value2;
     private IModel model;
     private List<IModel> modelArray;
+
+    public PModel() {
+    }
 
     public PModel(String value, Integer value2) {
       this.value = value;
@@ -60,22 +62,67 @@ public class IndexRequestProviderTest {
       return value;
     }
 
+    public void setValue(String value) {
+      this.value = value;
+    }
+
     public Integer getValue2() {
       return value2;
+    }
+
+    public void setValue2(Integer value2) {
+      this.value2 = value2;
     }
 
     public IModel getModel() {
       return model;
     }
 
+    public void setModel(IModel model) {
+      this.model = model;
+    }
+
     public List<IModel> getModelArray() {
       return modelArray;
+    }
+
+    public void setModelArray(List<IModel> modelArray) {
+      this.modelArray = modelArray;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) return true;
+      if (o == null || getClass() != o.getClass()) return false;
+      PModel pModel = (PModel) o;
+      return Objects.equals(value, pModel.value) &&
+              Objects.equals(value2, pModel.value2) &&
+              Objects.equals(model, pModel.model) &&
+              Objects.equals(modelArray, pModel.modelArray);
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(value, value2, model, modelArray);
+    }
+
+    @Override
+    public String toString() {
+      return "PModel{" +
+              "value='" + value + '\'' +
+              ", value2=" + value2 +
+              ", model=" + model +
+              ", modelArray=" + modelArray +
+              '}';
     }
   }
 
   public static final class IModel {
     private String value;
     private Integer value2;
+
+    public IModel() {
+    }
 
     public IModel(String value, Integer value2) {
       this.value = value;
@@ -86,8 +133,38 @@ public class IndexRequestProviderTest {
       return value;
     }
 
+    public void setValue(String value) {
+      this.value = value;
+    }
+
     public Integer getValue2() {
       return value2;
+    }
+
+    public void setValue2(Integer value2) {
+      this.value2 = value2;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) return true;
+      if (o == null || getClass() != o.getClass()) return false;
+      IModel iModel = (IModel) o;
+      return Objects.equals(value, iModel.value) &&
+              Objects.equals(value2, iModel.value2);
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(value, value2);
+    }
+
+    @Override
+    public String toString() {
+      return "IModel{" +
+              "value='" + value + '\'' +
+              ", value2=" + value2 +
+              '}';
     }
   }
 }
